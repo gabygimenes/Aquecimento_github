cd3bd4ce4dd262f790e3bc38fd8c11e9d9efbbd9
gabygimenes <gabriela.gimenes@edu.senai.br>
2026-08-06 15:37:03 -0300

    aquecimentoex4

---
+++ ex4.js
@@ -0,0 +1,8 @@
+const numeros = [5, 9, 12, 4, 18, 15];
+ const numerosEncontrados01 = numeros.find (numero => numero === 12);
+const numerosEncontrados02 = numeros.find (numero => numero === 18);
+const numerosEncontrados03 = numeros.find (numero => numero === 15);
+
+console.log (numerosEncontrados01);
+console.log (numerosEncontrados02);
+console.log (numerosEncontrados03);
