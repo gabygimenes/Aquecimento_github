4e0f31652247974879fc1203225b4bb57a8f71b0
gabygimenes <gabriela.gimenes@edu.senai.br>
2026-08-06 15:42:45 -0300

    aquecimentoex5

---
+++ ex5.js
@@ -0,0 +1,8 @@
+const estoque = [200, 450, 100, 900, 300];
+let amior = estoque[0];
+for (let i = 1; i < estoque.length; i++) {
+    if (estoque[i] > amior) {
+        amior = estoque[i];
+    }
+}
+    console.log(" O maior número do estoque é : " + amior);
