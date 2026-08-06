ddb0cc9d25d6f7892fa2c0d033a0c00cb8b2695e
gabygimenes <gabriela.gimenes@edu.senai.br>
2026-08-06 15:25:39 -0300

    aquecimentoex2

---
+++ ex2.js
@@ -0,0 +1,9 @@
+let num = [12, 67, 34, 89, 21];;
+
+let amior = num[0];
+for (let i = 1; i < num.length; i++) {
+    if (num[i] > amior) {
+        amior = num[i];
+    }
+}
+    console.log(" O maior número é : " + amior);
